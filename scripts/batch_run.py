c6813b2edf76c0af3535805d43532c1b36541a3d
Ian Fang <xf19@my.fsu.edu>
2020-06-24 13:17:00 -0500

    optimize batch run script structure

--- scripts/batch_run.py
+++ scripts/batch_run.py
@@ -2,40 +2,90 @@
 # coding: utf-8
 """Run combinations of models and dataset in batch"""
 
-# Module imports
-from pathlib import Path
-import pickle
 import json
+import pickle
+from pathlib import Path
 
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from collections import OrderedDict
-from scipy import stats
-from scipy.stats import skew, kurtosis
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.svm import SVC, LinearSVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, GradientBoostingClassifier
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import StratifiedKFold
-from sklearn.manifold import LocallyLinearEmbedding
-from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
-from sklearn.model_selection import learning_curve
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.linear_model import LogisticRegression
-from xgboost import XGBClassifier
 from mlxtend.classifier import StackingClassifier
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, \
+    GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC, LinearSVC
+from xgboost import XGBClassifier
 
 
 def main():
+
+    # utility functions
+    def select_channels(data_df, channels):
+        """Select a list of channels"""
+        return data_df.loc[(slice(None), slice(None), channels), :].unstack('channel')
+
+    def prep_data(dataframe):
+        """Prepare data from dataframe"""
+        X = dataframe.sample(frac=1)
+        y = dataframe.reset_index('healthy')['healthy']  # index 'healthy' is used as the class label
+        y = y ^ 1  # invert y labels
+        return X, y
+
+    def run_datasets(datasets, pipe_template, param_grids):
+        """
+        Batch run combinations
+        """
+        results = list()
+        test_results = list()
+        for ds_desc, ds in datasets:
+            print('Fitting model on dataset {}'.format(ds_desc))
+            X, y = prep_data(ds)
+            X_train, X_test, y_train, y_test = train_test_split(
+                X,
+                y,
+                test_size=TEST_SIZE,
+                stratify=y,
+                random_state=SEED
+            )
+            cls = Pipeline(pipe_template)
+            cv = list(StratifiedKFold(n_splits=CV, shuffle=True, random_state=SEED).split(X_train, y_train))
+            grid = GridSearchCV(
+                cls,
+                param_grid=param_grids,
+                n_jobs=1,
+                cv=cv,
+                refit=REFIT,
+                return_train_score=True,
+                scoring=SCORING
+            )
+            grid.fit(X_train, y_train)
+            cv_results = grid.cv_results_
+            runs = len(cv_results['params'])
+            for i in range(runs):
+                results.append(
+                    [ds_desc] +
+                    [GLOBAL_PARAMS] +
+                    [cv_results[f][i] for f in grid_fields]
+                )
+            y_pred = grid.best_estimator_.predict(X_test)
+            test_results.append(
+                [ds_desc] +
+                [GLOBAL_PARAMS] +
+                [grid.best_params_] +
+                [accuracy_score(y_test, y_pred),
+                 precision_score(y_test, y_pred),
+                 recall_score(y_test, y_pred),
+                 roc_auc_score(y_test, y_pred)
+                 ]
+            )
+        return results, test_results
+
     # Global configuration
     SEED = 24
     CV = 5
@@ -71,67 +121,6 @@ def main():
     test_results_file = root / 'test_result.csv'
     results = list()
     test_results = list()
-
-    # utility functions
-    def select_channels(data_df, channels):
-        """Select a list of channels"""
-        return data_df.loc[(slice(None), slice(None), channels), :].unstack('channel')
-
-    def prep_data(dataframe):
-        """Prepare data from dataframe"""
-        X = dataframe.sample(frac=1)
-        y = dataframe.reset_index('healthy')['healthy']  # index 'healthy' is used as the class label
-        y = y ^ 1  # invert y labels
-        return X, y
-
-    def run_datasets(datasets, pipe_template, param_grids):
-        """
-        Batch run combinations
-        """
-        results = list()
-        test_results = list()
-        for ds_desc, ds in datasets:
-            print('Fitting model on dataset {}'.format(ds_desc))
-            X, y = prep_data(ds)
-            X_train, X_test, y_train, y_test = train_test_split(
-                    X,
-                    y,
-                    test_size=TEST_SIZE,
-                    stratify=y,
-                    random_state=SEED
-            )
-            cls = Pipeline(pipe_template)
-            cv = list(StratifiedKFold(n_splits=CV, shuffle=True, random_state=SEED).split(X_train, y_train))
-            grid = GridSearchCV(
-                cls,
-                param_grid=param_grids,
-                n_jobs=1,
-                cv=cv,
-                refit=REFIT,
-                return_train_score=True,
-                scoring=SCORING
-            )
-            grid.fit(X_train, y_train)
-            cv_results = grid.cv_results_
-            runs = len(cv_results['params'])
-            for i in range(runs):
-                results.append(
-                    [ds_desc] +
-                    [GLOBAL_PARAMS] +
-                    [cv_results[f][i] for f in grid_fields]
-                    )
-            y_pred = grid.best_estimator_.predict(X_test)
-            test_results.append(
-                [ds_desc] +
-                [GLOBAL_PARAMS] +
-                [grid.best_params_] +
-                [accuracy_score(y_test, y_pred),
-                precision_score(y_test, y_pred),
-                recall_score(y_test, y_pred),
-                roc_auc_score(y_test, y_pred)
-                ]
-                )
-        return results, test_results
 
     # Load intermediate data
     unioned_feature_df_file = Path('/tmp/working/fang/data/unioned_feature_df.pkl')
